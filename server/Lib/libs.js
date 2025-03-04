f52d277c6a5cefe4e45ca931faec25f772d61095
dnyongesa244-pro <dnyongesa244@gmail.com>
2025-03-04 18:28:57 +0300

    Corrected a typo on cookies

--- server/Lib/libs.js
+++ server/Lib/libs.js
@@ -2,12 +2,12 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId, res) =>{
         const token = jwt.sign({userId}, process.env.JWT_SECRETE, {
-            expires:"10min"
+            expiresIn:"10m"
         });
         res.cookie("jwt", token, {
             maxAge : 1000*60*10,
             httpOnly : true,
-            sameSite : "strick",
+            sameSite : "strict",
             secure : process.env.NODE_EN !== "development"
         });
         return token;
